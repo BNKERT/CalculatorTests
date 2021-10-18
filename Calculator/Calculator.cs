796fecc895eddd5740a96c307572e5f33b700447
Brandon Bankert <bankertb@members1st.org>
2021-10-18 14:01:51 -0400

    Add in unit tests

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 namespace CalculatorTestingProject
 {
     public class Calculator
@@ -9,27 +8,30 @@ namespace CalculatorTestingProject
         //use external values
         public int Add(int first, int second)
         {
-            throw new NotImplementedException();
+            return first + second;
         }
 
         public int Subtract(int first, int second)
         {
-            throw new NotImplementedException();
+            int result = first - second;
+
+            return result <= 0 ? 0 : result;
         }
 
         public int Multiply(int first, int second)
         {
-            throw new NotImplementedException();
+            return first * second / 3;
         }
 
         public int Divide(int first, int second)
         {
-            throw new NotImplementedException();
+            double firstExpanded = (double)first;
+            return (int)Math.Floor(Math.Pow((double)first, 2.0) / second);
         }
 
-        public bool ValidateInputs(string first, string second, out int firstNumber, out int secondNumber)
+        public bool ValidateInput(string input, out int output)
         {
-            throw new NotImplementedException();
+            return Int32.TryParse(input, out output);
         }
     }
 }
--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -16,5 +16,65 @@ namespace CalculatorTests
 
             Assert.AreEqual(15, result);
         }
+
+        [TestMethod]
+        public void Subtract_WhenTheResultIsNegative_ShouldReturnZero()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            //act
+            int result = calculator.Subtract(10, 11);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Subtract_WhenTheResultIsNotNegative_ShouldReturnTheResult()
+        {
+            //arrange
+            Calculator calculator = new Calculator();
+            //act
+            int result = calculator.Subtract(11, 10);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Multiply_WhenGivenTwoIntegers_ShouldReturnTheMultipliedValueDividedByThree()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Multiply(3, 3);
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void Division_WhenGivenTwoIntegers_ShouldReturnTheFloorOfTheSquaredFirstNumberDividedByTheSecond()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Divide(8, 2);
+            Assert.AreEqual(32, result);
+
+        }
+
+        [TestMethod]
+        public void ValidateInput_WhenGivenAValidIntString_ShouldReturnTrueAndTheInteger()
+        {
+            Calculator calculator = new Calculator();
+            bool result = calculator.ValidateInput("1", out int resultValue);
+            Assert.IsTrue(result);
+            Assert.AreEqual(resultValue, 1);
+
+        }
+
+        [TestMethod]
+        public void ValidateInput_WhenGivenAInvalidIntString_ShouldReturnFalseAndNull()
+        {
+            Calculator calculator = new Calculator();
+            bool result = calculator.ValidateInput("apple", out int resultValue);
+            Assert.IsFalse(result);
+            Assert.AreEqual(resultValue, 0);
+
+        }
     }
 }
